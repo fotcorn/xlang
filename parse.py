57dcb75a003460c9c8276904d2ee81b9a607f932
fotcorn <fotcorn@gmail.com>
2019-10-16 17:12:23 +0200

    xl: add variable declaration, struct/enum access

--- parse.py
+++ parse.py
@@ -19,19 +19,21 @@ def main():
 
     IDENTIFIER: /[a-zA-Z_][a-zA-Z_0-9]*/
     INTEGER: /[1-9][0-9]*/
+    var_access: IDENTIFIER ("." IDENTIFIER)*
 
     code_block: "{" statement* "}"
 
-    statement: loop | if | (function_call ";") | variable_def | variable_assign | control ";"
+    statement: loop | if | (function_call ";") | variable_dec | variable_def | variable_assign | control ";"
     loop: "loop" code_block
     if: "if" code_block
 
+    variable_dec: IDENTIFIER ":" type ";"
     variable_def: IDENTIFIER ":" type "=" compare_expr ";"
-    variable_assign: IDENTIFIER "=" compare_expr ";"
+    variable_assign: var_access "=" compare_expr ";"
     !control: ("break" | "continue" | "return" compare_expr?)
 
     function_call: IDENTIFIER "(" ")"
-    primary_expression: IDENTIFIER | function_call | INTEGER
+    primary_expression: var_access | function_call | INTEGER
 
     !?mul_div_expr: primary_expression | (mul_div_expr "*" primary_expression) | (mul_div_expr "/" primary_expression)
     !?add_sub_expr: mul_div_expr | (add_sub_expr "+" mul_div_expr) | (add_sub_expr "-" mul_div_expr)
@@ -45,7 +47,7 @@ def main():
     %ignore WS
     '''
 
-    with open('test.xl') as f:
+    with open('tests/struct_access.xl') as f:
         code = f.read()
 
     lark = Lark(grammar, parser='lalr')

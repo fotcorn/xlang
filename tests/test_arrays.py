e24a141f20127b2d116d28149d3bf455a1788469
fotcorn <fotcorn@gmail.com>
2020-10-30 22:29:47 +0000

    new array definition syntax

--- tests/test_arrays.py
+++ tests/test_arrays.py
@@ -2,17 +2,14 @@ from xlang.xl_ast import GlobalScope
 from xlang.parser import Parser
 
 
-def test_hello(parser: Parser):
+def test_definition(parser: Parser):
     ast: GlobalScope = parser.parse(
         """
         main() {
-            array: [int];
-            array.add(5);
-            print(array[0]);
+            array: [int] = 5;
         }
         """
     )
 
     assert len(ast.functions) == 1
-    assert len(ast.structs) == 1
     assert "main" in ast.functions

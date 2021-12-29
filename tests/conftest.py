7fe5ebbf3bccd2f5f3e804b5b2aa6e6b4abf75fc
fotcorn <fotcorn@gmail.com>
2021-12-29 22:39:37 +0000

    tests: move run to conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,18 @@
 import pytest
 from xlang.parser import Parser
+from xlang.xl_ast import GlobalScope
+from xlang.interpreter import Interpreter
+from xlang.validation_pass import validation_pass
 
 
 @pytest.fixture
 def parser():
     return Parser()
+
+
+def run(code):
+    parser = Parser()
+    ast: GlobalScope = parser.parse(code)
+    validation_pass(ast)
+    interpreter = Interpreter()
+    interpreter.run(ast)
--- tests/test_interpreter.py
+++ tests/test_interpreter.py
@@ -1,18 +1,7 @@
 from xlang.exceptions import InterpreterAssertionError
-from xlang.xl_ast import GlobalScope
-from xlang.parser import Parser
-from xlang.interpreter import Interpreter
-from xlang.validation_pass import validation_pass
+from .conftest import run
 
 import pytest
-
-
-def run(code):
-    parser = Parser()
-    ast: GlobalScope = parser.parse(code)
-    validation_pass(ast)
-    interpreter = Interpreter()
-    interpreter.run(ast)
 
 
 def test_hello():

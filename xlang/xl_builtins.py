4db89b8426c158cb84cf3cc4b68658a6624cdab1
fotcorn <fotcorn@gmail.com>
2022-10-30 10:38:50 +0000

    refactor builtins to use decorator

--- xlang/xl_builtins.py
+++ xlang/xl_builtins.py
@@ -1,3 +1,4 @@
+import functools
 from typing import List
 from xlang.exceptions import InterpreterAssertionError
 from xlang.xl_ast import (
@@ -10,9 +11,61 @@ from xlang.xl_ast import (
 )
 from xlang.interpreter import Value
 
+BUILTINS = []
 
-def print_builtin(values: List[Value], *args, **kwargs):
-    print(values[0].value)
+
+def builtin(name, return_type, params):
+    def decorate(func):
+        global BUILTINS
+
+        @functools.wraps(func)
+        def wrapper(params, **kwargs):
+            return func(*params)
+
+        BUILTINS.append(
+            BuiltinFunction(
+                name,
+                return_type,
+                params,
+                wrapper,
+            )
+        )
+
+        return wrapper
+
+    return decorate
+
+
+def prim(param_name: str, primitive_type: PrimitiveType):
+    return FunctionParameter(
+        name=param_name,
+        context=ParseContext(None),
+        param_type=VariableType(
+            variable_type=VariableTypeEnum.PRIMITIVE,
+            primitive_type=primitive_type,
+        ),
+        reference=False,
+    )
+
+
+@builtin("prints", None, [prim("value", PrimitiveType.STRING)])
+def builtin_prints(value: Value):
+    print(value.value)
+
+
+@builtin("printi", None, [prim("value", PrimitiveType.I64)])
+def builtin_printi(value: Value):
+    print(value.value)
+
+
+@builtin("printf", None, [prim("value", PrimitiveType.FLOAT)])
+def builtin_printf(value: Value):
+    print(value.value)
+
+
+@builtin("printb", None, [prim("value", PrimitiveType.BOOL)])
+def builtin_printb(value: Value):
+    print(value.value)
 
 
 def append_builtin(values: List[Value], *args, **kwargs):
@@ -28,170 +81,109 @@ def assert_builtin(values: List[Value], context, *args, **kwargs):
         raise InterpreterAssertionError("assertion failed", context)
 
 
+BUILTINS += [
+    BuiltinFunction(
+        "appendi",
+        None,
+        [
+            FunctionParameter(
+                name="array",
+                context=ParseContext(None),
+                param_type=VariableType(
+                    variable_type=VariableTypeEnum.ARRAY,
+                    array_type=VariableType(
+                        variable_type=VariableTypeEnum.PRIMITIVE,
+                        primitive_type=PrimitiveType.I64,
+                    ),
+                ),
+                reference=True,
+            ),
+            FunctionParameter(
+                name="value",
+                context=ParseContext(None),
+                param_type=VariableType(
+                    variable_type=VariableTypeEnum.PRIMITIVE,
+                    primitive_type=PrimitiveType.I64,
+                ),
+                reference=False,
+            ),
+        ],
+        append_builtin,
+    ),
+    BuiltinFunction(
+        "appends",
+        None,
+        [
+            FunctionParameter(
+                name="array",
+                context=ParseContext(None),
+                param_type=VariableType(
+                    variable_type=VariableTypeEnum.ARRAY,
+                    array_type=VariableType(
+                        variable_type=VariableTypeEnum.PRIMITIVE,
+                        primitive_type=PrimitiveType.STRING,
+                    ),
+                ),
+                reference=True,
+            ),
+            FunctionParameter(
+                name="value",
+                context=ParseContext(None),
+                param_type=VariableType(
+                    variable_type=VariableTypeEnum.PRIMITIVE,
+                    primitive_type=PrimitiveType.STRING,
+                ),
+                reference=False,
+            ),
+        ],
+        append_builtin,
+    ),
+    BuiltinFunction(
+        "appendf",
+        None,
+        [
+            FunctionParameter(
+                name="array",
+                context=ParseContext(None),
+                param_type=VariableType(
+                    variable_type=VariableTypeEnum.ARRAY,
+                    array_type=VariableType(
+                        variable_type=VariableTypeEnum.PRIMITIVE,
+                        primitive_type=PrimitiveType.FLOAT,
+                    ),
+                ),
+                reference=True,
+            ),
+            FunctionParameter(
+                name="value",
+                context=ParseContext(None),
+                param_type=VariableType(
+                    variable_type=VariableTypeEnum.PRIMITIVE,
+                    primitive_type=PrimitiveType.FLOAT,
+                ),
+                reference=False,
+            ),
+        ],
+        append_builtin,
+    ),
+    BuiltinFunction(
+        "assert",
+        None,
+        [
+            FunctionParameter(
+                name="test",
+                context=ParseContext(None),
+                param_type=VariableType(
+                    variable_type=VariableTypeEnum.PRIMITIVE,
+                    primitive_type=PrimitiveType.BOOL,
+                ),
+                reference=False,
+            )
+        ],
+        assert_builtin,
+    ),
+]
+
+
 def get_builtins() -> List[BuiltinFunction]:
-    return [
-        BuiltinFunction(
-            "printi",
-            None,
-            [
-                FunctionParameter(
-                    name="value",
-                    context=ParseContext(None),
-                    param_type=VariableType(
-                        variable_type=VariableTypeEnum.PRIMITIVE,
-                        primitive_type=PrimitiveType.I64,
-                    ),
-                    reference=False,
-                )
-            ],
-            print_builtin,
-        ),
-        BuiltinFunction(
-            "prints",
-            None,
-            [
-                FunctionParameter(
-                    name="value",
-                    context=ParseContext(None),
-                    param_type=VariableType(
-                        variable_type=VariableTypeEnum.PRIMITIVE,
-                        primitive_type=PrimitiveType.STRING,
-                    ),
-                    reference=False,
-                )
-            ],
-            print_builtin,
-        ),
-        BuiltinFunction(
-            "printf",
-            None,
-            [
-                FunctionParameter(
-                    name="value",
-                    context=ParseContext(None),
-                    param_type=VariableType(
-                        variable_type=VariableTypeEnum.PRIMITIVE,
-                        primitive_type=PrimitiveType.FLOAT,
-                    ),
-                    reference=False,
-                )
-            ],
-            print_builtin,
-        ),
-        BuiltinFunction(
-            "printb",
-            None,
-            [
-                FunctionParameter(
-                    name="value",
-                    context=ParseContext(None),
-                    param_type=VariableType(
-                        variable_type=VariableTypeEnum.PRIMITIVE,
-                        primitive_type=PrimitiveType.BOOL,
-                    ),
-                    reference=False,
-                )
-            ],
-            print_builtin,
-        ),
-        BuiltinFunction(
-            "appendi",
-            None,
-            [
-                FunctionParameter(
-                    name="array",
-                    context=ParseContext(None),
-                    param_type=VariableType(
-                        variable_type=VariableTypeEnum.ARRAY,
-                        array_type=VariableType(
-                            variable_type=VariableTypeEnum.PRIMITIVE,
-                            primitive_type=PrimitiveType.I64,
-                        ),
-                    ),
-                    reference=True,
-                ),
-                FunctionParameter(
-                    name="value",
-                    context=ParseContext(None),
-                    param_type=VariableType(
-                        variable_type=VariableTypeEnum.PRIMITIVE,
-                        primitive_type=PrimitiveType.I64,
-                    ),
-                    reference=False,
-                ),
-            ],
-            append_builtin,
-        ),
-        BuiltinFunction(
-            "appends",
-            None,
-            [
-                FunctionParameter(
-                    name="array",
-                    context=ParseContext(None),
-                    param_type=VariableType(
-                        variable_type=VariableTypeEnum.ARRAY,
-                        array_type=VariableType(
-                            variable_type=VariableTypeEnum.PRIMITIVE,
-                            primitive_type=PrimitiveType.STRING,
-                        ),
-                    ),
-                    reference=True,
-                ),
-                FunctionParameter(
-                    name="value",
-                    context=ParseContext(None),
-                    param_type=VariableType(
-                        variable_type=VariableTypeEnum.PRIMITIVE,
-                        primitive_type=PrimitiveType.STRING,
-                    ),
-                    reference=False,
-                ),
-            ],
-            append_builtin,
-        ),
-        BuiltinFunction(
-            "appendf",
-            None,
-            [
-                FunctionParameter(
-                    name="array",
-                    context=ParseContext(None),
-                    param_type=VariableType(
-                        variable_type=VariableTypeEnum.ARRAY,
-                        array_type=VariableType(
-                            variable_type=VariableTypeEnum.PRIMITIVE,
-                            primitive_type=PrimitiveType.FLOAT,
-                        ),
-                    ),
-                    reference=True,
-                ),
-                FunctionParameter(
-                    name="value",
-                    context=ParseContext(None),
-                    param_type=VariableType(
-                        variable_type=VariableTypeEnum.PRIMITIVE,
-                        primitive_type=PrimitiveType.FLOAT,
-                    ),
-                    reference=False,
-                ),
-            ],
-            append_builtin,
-        ),
-        BuiltinFunction(
-            "assert",
-            None,
-            [
-                FunctionParameter(
-                    name="test",
-                    context=ParseContext(None),
-                    param_type=VariableType(
-                        variable_type=VariableTypeEnum.PRIMITIVE,
-                        primitive_type=PrimitiveType.BOOL,
-                    ),
-                    reference=False,
-                )
-            ],
-            assert_builtin,
-        ),
-    ]
+    return BUILTINS

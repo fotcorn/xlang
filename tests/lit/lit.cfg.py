cfac9f84f51a855679baabb7a5237a420e37ff2c
fotcorn <fotcorn@gmail.com>
2023-09-26 00:03:59 +0100

    add minimal lit test

---
+++ tests/lit/lit.cfg.py
@@ -0,0 +1,16 @@
+import os
+import lit.formats
+from lit import TestingConfig
+
+config: TestingConfig
+
+config.name = "xlang"
+
+config.test_format = lit.formats.ShTest(True)
+
+config.suffixes = [".xl"]
+
+lit_cfg_dir = os.path.dirname(__file__)
+main_py_path = os.path.abspath(os.path.join(lit_cfg_dir, "../../main.py"))
+
+config.substitutions.append(("%run", f"python {main_py_path}"))

cda222443d9b9b56a78ca3f01a949c992ff42247
fotcorn <fotcorn@gmail.com>
2023-09-26 11:48:49 +0100

    use FileCheck from llvm

--- tests/lit/lit.cfg.py
+++ tests/lit/lit.cfg.py
@@ -1,4 +1,7 @@
 import os
+import shutil
+import subprocess
+
 import lit.formats
 from lit import TestingConfig
 
@@ -14,3 +17,14 @@ lit_cfg_dir = os.path.dirname(__file__)
 main_py_path = os.path.abspath(os.path.join(lit_cfg_dir, "../../main.py"))
 
 config.substitutions.append(("%run", f"python {main_py_path}"))
+
+# hook up FileCheck
+filecheck_bin = shutil.which("FileCheck")
+if not filecheck_bin:
+    for i in range(15, 11, -1):
+        filecheck_bin = shutil.which(f"FileCheck-{i}")
+        if filecheck_bin:
+            break
+if not filecheck_bin:
+    raise Exception("FileCheck binary not found in PATH")
+config.substitutions.append(("%filecheck", f"{filecheck_bin} --match-full-lines"))

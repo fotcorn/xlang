c1ec3829f8d571c3019634dcc20cce1b29a2fe9b
fotcorn <fotcorn@gmail.com>
2023-09-26 21:04:52 +0100

    move more tests to lit

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -3,70 +3,6 @@ from .conftest import run
 import pytest
 
 
-def test_function_int_return():
-    run(
-        """
-        myfunc(): int {
-            return 5;
-        }
-        main() {
-            i: int = myfunc();
-            assert(i == 5);
-        }
-    """
-    )
-
-
-def test_function_struct_return():
-    run(
-        """
-        struct MyStruct {
-            a: int,
-            b: int,
-        }
-
-        myfunc(): MyStruct {
-            s: MyStruct;
-            s.a = 5;
-            s.b = 7;
-            return s;
-        }
-        main() {
-            s: MyStruct = myfunc();
-            assert(s.a == 5);
-            assert(s.b == 7);
-        }
-    """
-    )
-
-
-def test_function_struct_param_copy():
-    run(
-        """
-        struct MyStruct {
-            a: int,
-            b: int,
-        }
-
-        myfunc(s: MyStruct) {
-            assert(s.a == 5);
-            assert(s.b == 7);
-            s.a = 11;
-            s.b = 12;
-        }
-
-        main() {
-            s: MyStruct;
-            s.a = 5;
-            s.b = 7;
-            myfunc(s);
-            assert(s.a == 5);
-            assert(s.b == 7);
-        }
-        """
-    )
-
-
 def test_duplicate_function():
     with pytest.raises(FunctionAlreadyDefinedException):
         run(
@@ -75,31 +11,6 @@ def test_duplicate_function():
             a() {}
             """
         )
-
-
-def test_reference_func():
-    run(
-        """
-        struct X {
-            a: int,
-        }
-        func_ref(x: *X) {
-            x.a = 5;
-        }
-        func_copy(x: X) {
-            x.a = 6;
-        }
-        main() {
-            x: X;
-            x.a = 1;
-            func_ref(x);
-            assert(x.a == 5);
-            x.a = 2;
-            func_copy(x);
-            assert(x.a == 2);
-        }
-        """
-    )
 
 
 def test_fail_ref_param():

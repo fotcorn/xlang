91e4af6ebc349a5559a4d8dd1c415257e7ff8e99
fotcorn <fotcorn@gmail.com>
2023-09-26 11:49:40 +0100

    add split-input-file option to main for testing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import argparse
 import sys
+import re
 
 from xlang.parser import Parser
 from xlang.interpreter import Interpreter
@@ -8,19 +9,7 @@ from xlang.validation_pass import validation_pass
 from xlang.exceptions import ContextException
 
 
-if __name__ == "__main__":
-    arg_parser = argparse.ArgumentParser(description="xlang interpreter")
-    arg_parser.add_argument("file", type=str)
-
-    args = arg_parser.parse_args()
-
-    try:
-        with open(args.file, "r") as f:
-            code = f.read()
-    except FileNotFoundError:
-        print("Code file does not exist")
-        sys.exit(1)
-
+def run(code: str):
     try:
         parser = Parser()
         ast: GlobalScope = parser.parse(code)
@@ -33,3 +22,25 @@ if __name__ == "__main__":
     except Exception as ex:
         print(ex)
         sys.exit(1)
+
+
+if __name__ == "__main__":
+    arg_parser = argparse.ArgumentParser(description="xlang interpreter")
+    arg_parser.add_argument("file")
+    arg_parser.add_argument("--split-input-file", action="store_true")
+
+    args = arg_parser.parse_args()
+
+    try:
+        with open(args.file, "r") as f:
+            code = f.read()
+    except FileNotFoundError:
+        print("Code file does not exist")
+        sys.exit(1)
+
+    if args.split_input_file:
+        code_chunks = re.split("^//-{3,}$", code, flags=re.MULTILINE)
+        for chunk in code_chunks:
+            run(chunk)
+    else:
+        run(code)
